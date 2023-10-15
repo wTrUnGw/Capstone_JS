d0bbd11eef664faa92e796c362792736d728bc8e
Dev1 <hoangphuc2420@gmail.com>
2023-10-15 16:07:14 +0700

    push JS

---
+++ admin/product.js
@@ -0,0 +1,8 @@
+function Product(_id, _tenSP, _gia, _hinhAnh, _chatLieu, _moTa) {
+  this.id = _id;
+  this.tenSP = _tenSP;
+  this.gia = _gia;
+  this.hinhAnh = _hinhAnh;
+  this.chatLieu = _chatLieu;
+  this.moTa = _moTa;
+}
